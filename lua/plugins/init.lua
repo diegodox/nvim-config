693a8cdc7c8b50a4b431069b6e9da32728587e64
Diego Fujii <android.mxdiego9@gmail.com>
2022-04-24 01:01:29 +0900

    configure lua lsp for nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -186,6 +186,19 @@ return require("packer").startup(function(use)
 
             lsp_installer.on_server_ready(function(server)
                 local opts = {}
+                if server.name == "sumneko_lua" then
+                    opts.settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = { 'vim' },
+                            },
+                            workspace = {
+                                -- Make the server aware of Neovim runtime files
+                                library = vim.api.nvim_get_runtime_file("", true),
+                            },
+                        },
+                    }
+                end
                 server:setup(opts)
                 vim.cmd([[do User LspAttachBuffers]])
             end)
