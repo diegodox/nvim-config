553f99b18be297b35ad048fe2a16010bb54cac97
Diego Fujii <android.mxdiego9@gmail.com>
2022-04-24 03:00:15 +0900

    add telescope lua keymap

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -74,10 +74,28 @@ function M.set_keymap()
         "<Cmd>lua require('telescope.builtin').buffers()<CR>",
         {noremap = true, silent = true}
     )
+    vim.api.nvim_set_keymap(
+        "n",
+        "<F2>",
+        "<Cmd>lua vim.lsp.buf.rename()<CR>",
+        {noremap = true, silent = true}
+    )
+    require('which-key').register(
+        {
+            d = { "<Cmd>Telescope lsp_definitions<CR>", "Definitions" },
+            t = { "<Cmd>Telescope lsp_type_definitions<CR>", "Type definitions" },
+            I = { "<Cmd>Telescope lsp_implementations<CR>", "Goto implementation" },
+            r = { "<Cmd>Telescope lsp_references<CR>", "References" },
+            ["@"] = { "<Cmd>Telescope lsp_document_symbols<CR>", "Document Symbols" },
+            w = { "<Cmd>Telescope lsp_document_symbols<CR>", "Workspace Document Symbols" },
+            ["."] = { "<Cmd>Telescope lsp_code_actions<CR>", "Code Action" },
+            [","] = { "<Cmd>Telescope lsp_range_code_actions<CR>", "Range Code Action" },
+        },
+        { prefix = "g" }
+    )
 end
 
--- configure telescope
--- setup, keymap
+-- configure telescope (setup, keymap)
 function M.config()
     M.setup()
     M.set_keymap()
