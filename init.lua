a5e25d5e97f31077db73e3ec0dca38d45300227c
Diego Fujii <android.mxdiego9@gmail.com>
2022-04-24 13:11:37 +0900

    clean init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
--- require`dein`
--- require`commands`
--- vim.cmd [[set runtimepath+=/home/diego/nvim-config]]
 require('rc.settings')
 require('rc.keymap')
 require('rc.plugins')
--- require'tabline'
--- require'lspconfig'
 
