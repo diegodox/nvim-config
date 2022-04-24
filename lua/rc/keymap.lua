2e8d33dfddac0221ecf6fb81495e8d5f1da5d642
Diego Fujii <android.mxdiego9@gmail.com>
2022-04-24 13:11:45 +0900

    comment keymap

--- lua/rc/keymap.lua
+++ lua/rc/keymap.lua
@@ -2,12 +2,15 @@
 vim.api.nvim_set_keymap( 'n', "<Esc>", "<cmd>call system('fcitx5-remote -o > /dev/null 2>&1')<CR><Esc>", { silent = true, noremap = true } )
 vim.api.nvim_set_keymap( 'i', "<Esc>", "<cmd>call system('fcitx5-remote -o > /dev/null 2>&1')<CR><Esc>", { silent = true, noremap = true } )
 
+-- Move visual line
 vim.api.nvim_set_keymap( 'n', "k", "gk", { silent = true, noremap = true } )
 vim.api.nvim_set_keymap( 'n', "j", "gj", { silent = true, noremap = true } )
 
+-- Keep select while indentation
 vim.api.nvim_set_keymap( 'v', '>', ">gv", { silent = true, noremap = true } )
 vim.api.nvim_set_keymap( 'v', '<', "<gv", { silent = true, noremap = true } )
 
+-- No arrowkeys
 vim.api.nvim_set_keymap( 'n', '<up>', '<nop>', { silent = true, noremap = true } )
 vim.api.nvim_set_keymap( 'n', '<down>', '<nop>', { silent = true, noremap = true } )
 vim.api.nvim_set_keymap( 'n', '<left>', '<nop>', { silent = true, noremap = true } )
