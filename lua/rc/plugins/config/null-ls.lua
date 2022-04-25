867be4dba94ca24222eeaf19d9bc0a0cdcd66f6f
Diego Fujii <android.mxdiego9@gmail.com>
2022-04-25 12:43:24 +0900

    auto format lua with StyLua via null-ls

--- lua/rc/plugins/config/null-ls.lua
+++ lua/rc/plugins/config/null-ls.lua
@@ -12,8 +12,17 @@ function M.config()
 	end
 	null_ls.setup({
 		sources = {
+			null_ls.builtins.formatting.stylua,
 		},
 		on_attach = function(client)
+			if client.resolved_capabilities.document_formatting then
+				vim.cmd([[
+                augroup LspFormatting
+                    autocmd! * <buffer>
+                    autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+                augroup END
+                ]])
+			end
 		end,
 	})
 end
