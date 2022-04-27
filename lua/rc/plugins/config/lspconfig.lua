ba7df9d270cba65eb19af893e90c7a49eace504d
Diego Fujii <android.mxdiego9@gmail.com>
2022-04-27 19:32:14 +0900

    add lsp keybind

--- lua/rc/plugins/config/lspconfig.lua
+++ lua/rc/plugins/config/lspconfig.lua
@@ -15,7 +15,19 @@ function M.def_sign()
 end
 
 function M.keymap()
-    require("which-key").register({ k = { "<Cmd>lua vim.lsp.buf.hover()<CR>", "Hover" } }, { prefix = "<Leader>" })
+    require("which-key").register({
+        k = { "<Cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
+        ["<C-k>"] = { "<Cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature" },
+    }, { prefix = "<Leader>" })
+    require("which-key").register({
+        l = {
+            name = "lsp",
+            h = { "<Cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
+            s = { "<Cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature" },
+            ["."] = { "<Cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
+            [","] = { "<Cmd>lua vim.lsp.buf.range_code_action()<CR>", "Range Code Action" },
+        },
+    }, { prefix = "<Leader>" })
     require("which-key").register({ ["<F2>"] = { "<Cmd>lua vim.lsp.buf.rename()<CR>", "Rename" } })
 end
 
