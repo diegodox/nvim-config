340e5a5fb1d1dc7a2f02f3ca947c85d7a51811f8
Diego Fujii <android.mxdiego9@gmail.com>
2022-04-27 16:09:43 +0900

    lualine make rich

--- lua/rc/plugins/config/lualine.lua
+++ lua/rc/plugins/config/lualine.lua
@@ -8,6 +8,16 @@ lualine_conf.requires = {
     "kyazdani42/nvim-web-devicons",
 }
 
+local function replace_empty(replaced)
+    return function(str)
+        if str == "" then
+            return replaced
+        else
+            return str
+        end
+    end
+end
+
 function lualine_conf.config()
     local gps = require("nvim-gps")
 
@@ -17,13 +27,77 @@ function lualine_conf.config()
 
     require("lualine").setup({
         options = {
-            globalstatus = true,
             section_separators = { left = "", right = "" },
         },
         sections = {
+            lualine_a = { "mode" },
+            lualine_b = {},
             lualine_c = {
+                {
+                    "filetype",
+                    icon_only = true,
+                    separator = "",
+                    padding = { right = 0, left = 1 },
+                },
+                "filename",
+            },
+            lualine_x = { "diff", "diagnostics" },
+            lualine_y = {},
+            lualine_z = {},
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {
+                {
+                    "filetype",
+                    icon_only = true,
+                    colors = false,
+                    separator = "",
+                    padding = { right = 0, left = 1 },
+                },
+                "filename",
+            },
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {},
+        },
+        tabline = {
+            lualine_a = { { "branch", fmt = replace_empty("-") } },
+            lualine_b = { "encoding", "fileformat", "filetype" },
+            lualine_c = {
+                {
+                    -- center this section
+                    "%=",
+                    separator = "",
+                    padding = { right = 0, left = 0 },
+                },
+                {
+                    "filetype",
+                    icon_only = true,
+                    separator = "",
+                    padding = { right = 0, left = 1 },
+                },
                 "filename",
                 { gps.get_location, cond = gps.is_available },
+            },
+            lualine_x = { "diff" },
+            lualine_y = { "location" },
+            lualine_z = {
+                {
+                    -- current working directory
+                    "vim.fn.getcwd()",
+                    fmt = function(cwd)
+                        return vim.fn.pathshorten(vim.fn.substitute(cwd, os.getenv("HOME"), "~", ""))
+                    end,
+                },
+                {
+                    -- current session
+                    "vim.v.this_session",
+                    fmt = function(session_path)
+                        return replace_empty("[NO SESSION]")(vim.fn.fnamemodify(session_path, ":t"))
+                    end,
+                },
             },
         },
     })
