1da6ba34f2202bce7027ea186ee211c4ec0d00ee
Diego Fujii <android.mxdiego9@gmail.com>
2022-04-28 13:29:07 +0900

    add toggleterm, toggle the terminal

--- lua/rc/plugins/config/toggleterm.lua
+++ lua/rc/plugins/config/toggleterm.lua
@@ -7,7 +7,54 @@ function M.config()
         vim.o.hidden = true
     end
     require("toggleterm").setup()
+    M.term()
     M.lazygit()
+end
+
+-- setup toggleterminal
+function M.term()
+    local Terminal = require("toggleterm.terminal").Terminal
+    local term = Terminal:new({
+        size = 15,
+        hidden = true,
+    })
+    function _TOGGLE_TERM()
+        term:toggle()
+    end
+
+    function _TOGGLE_TERM_VERTICAL()
+        local is_open = term:is_open()
+        if is_open then
+            term:close()
+        end
+        term:open(vim.o.columns * 0.4, "vertical", false)
+    end
+
+    function _TOGGLE_TERM_HORIZONTAL()
+        local is_open = term:is_open()
+        if is_open then
+            term:close()
+        end
+        term:open(15, "horizontal", false)
+    end
+
+    local whichkey = require("which-key")
+    whichkey.register({
+        ["<C-t>"] = {
+            name = "ToggleTerm",
+            ["<CR>"] = { "<cmd>lua _TOGGLE_TERM()<CR>", "Open Term" },
+            v = { "<cmd>lua _TOGGLE_TERM_VERTICAL()<CR>", "Open Vertical Terminal" },
+            s = { "<cmd>lua _TOGGLE_TERM_HORIZONTAL()<CR>", "Open Horizontal Terminal" },
+        },
+    })
+    whichkey.register({
+        ["<C-t>"] = {
+            name = "ToggleTerm",
+            ["<CR>"] = { "<cmd>lua _TOGGLE_TERM()<CR>", "Close Term" },
+            v = { "<cmd>lua _TOGGLE_TERM_VERTICAL()<CR>", "Move Terminal Vertical" },
+            s = { "<cmd>lua _TOGGLE_TERM_HORIZONTAL()<CR>", "Open Terminal Horizontal" },
+        },
+    }, { mode = "t" })
 end
 
 -- setup lazygit toggleterm
