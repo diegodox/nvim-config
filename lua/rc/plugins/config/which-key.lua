c0929c8df5cc66fee5c7a911dedfa9f1ce9d136b
Diego Fujii <android.mxdiego9@gmail.com>
2022-05-04 13:52:28 +0900

    lualine fix icon only

--- lua/rc/plugins/config/which-key.lua
+++ lua/rc/plugins/config/which-key.lua
@@ -7,7 +7,7 @@ end
 function M.config()
     local ok, whichkey = pcall(require, "which-key")
     if not ok then
-        vim.notify_once("plugin 'which-key' not found, skip setup 'which-key'", vim.lsp.log_levels.ERROR)
+        vim.notify_once("Plugin 'which-key' not found\nSkip setup 'which-key'", vim.lsp.log_levels.ERROR)
         return
     end
     whichkey.setup({})
