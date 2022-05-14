4438492c8076b116ad1d879e0be4da8571e98ced
Diego Fujii <android.mxdiego9@gmail.com>
2022-05-14 17:38:54 +0900

    update toggleterm

--- lua/rc/plugins/config/toggleterm.lua
+++ lua/rc/plugins/config/toggleterm.lua
@@ -6,9 +6,13 @@ function M.config()
         vim.notify("toggleterm needs 'hidden' option, set hidden = true", vim.log.levels.WARN)
         vim.o.hidden = true
     end
+    -- error: Normalにリンクされるだろうハイライトの背景が真っ黒になる？
+    --        Notifyの影響かも？
+    -- require("toggleterm").setup({ float_opts = { winblend = 4 } })
     require("toggleterm").setup()
     M.term()
     M.lazygit()
+    M.ranger()
 end
 
 -- setup toggleterminal
@@ -17,74 +21,94 @@ function M.term()
     local term = Terminal:new({
         size = 15,
         hidden = true,
+        env = {
+            -- open a new split in current nvim to edit, instead of opening new vim in terminal
+            VISUAL = "nvr -cc split",
+            EDITOR = "nvr -cc split",
+        },
     })
-    function _TOGGLE_TERM()
-        term:toggle()
-    end
 
-    function _TOGGLE_TERM_VERTICAL()
+    vim.api.nvim_create_user_command("ToggleTerm", function()
+        ---@diagnostic disable-next-line: missing-parameter
+        term:toggle()
+    end, { desc = "Toggle terminal" })
+
+    vim.api.nvim_create_user_command("ToggleTermVertical", function()
         local is_open = term:is_open()
         if is_open then
             term:close()
         end
         term:open(vim.o.columns * 0.4, "vertical", false)
-    end
+    end, { desc = "Toggle terminal open in vertical" })
 
-    function _TOGGLE_TERM_HORIZONTAL()
+    vim.api.nvim_create_user_command("ToggleTermHorizontal", function()
         local is_open = term:is_open()
         if is_open then
             term:close()
         end
         term:open(15, "horizontal", false)
+    end, { desc = "Toggle terminal open in horizontal" })
+
+    local ok_whichkey, whichkey = pcall(require, "which-key")
+    if ok_whichkey then
+        whichkey.register({ ["<C-t>"] = { name = "ToggleTerm" } })
+        whichkey.register({ ["<C-t>"] = { name = "ToggleTerm" } }, { mode = "t" })
+    else
+        vim.notify_once("Plugin 'which-key' not found\nSetup keymap without 'which-key'", vim.lsp.log_levels.INFO)
     end
 
-    local whichkey = require("which-key")
-    whichkey.register({
-        ["<C-t>"] = {
-            name = "ToggleTerm",
-            ["<CR>"] = { "<cmd>lua _TOGGLE_TERM()<CR>", "Open Term" },
-            ["<C-CR>"] = { "<cmd>lua _TOGGLE_TERM()<CR>", "Open Term" },
-            v = { "<cmd>lua _TOGGLE_TERM_VERTICAL()<CR>", "Open Vertical Terminal" },
-            s = { "<cmd>lua _TOGGLE_TERM_HORIZONTAL()<CR>", "Open Horizontal Terminal" },
-        },
-    })
-    whichkey.register({
-        ["<C-t>"] = {
-            name = "ToggleTerm",
-            ["<CR>"] = { "<cmd>lua _TOGGLE_TERM()<CR>", "Close Term" },
-            ["<C-CR>"] = { "<cmd>lua _TOGGLE_TERM()<CR>", "Close Term" },
-            v = { "<cmd>lua _TOGGLE_TERM_VERTICAL()<CR>", "Move Terminal Vertical" },
-            s = { "<cmd>lua _TOGGLE_TERM_HORIZONTAL()<CR>", "Open Terminal Horizontal" },
-        },
-    }, { mode = "t" })
+    vim.keymap.set("n", "<C-t><CR>", "<cmd>ToggleTerm<CR>", { desc = "Toglle Terminal" })
+    vim.keymap.set("n", "<C-t><C-CR>", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+    vim.keymap.set("n", "<C-t>v", "<cmd>ToggleTermVertical<CR>", { desc = "Toggle Terminal Vertically" })
+    vim.keymap.set("n", "<C-t>s", "<cmd>ToggleTermHorizontal<CR>", { desc = "Toggle Terminal Horizontally" })
+
+    vim.keymap.set("t", "<C-t><CR>", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+    vim.keymap.set("t", "<C-t><C-CR>", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+    vim.keymap.set("t", "<C-t>v", "<cmd>ToggleTermVertical<CR>", { desc = "Toggle Terminal Vertically" })
+    vim.keymap.set("t", "<C-t>s", "<cmd>ToggleTermHorizontal<CR>", { desc = "Toggle Terminal Horizontally" })
 end
 
 -- setup lazygit toggleterm
 function M.lazygit()
-    local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({
+    local lazygit = require("toggleterm.terminal").Terminal:new({
         cmd = "lazygit",
         direction = "float",
         hidden = true,
+        env = {
+            -- open a new tab in current nvim to edit, instead of opening new vim in lazygit
+            VISUAL = "nvr --remote-tab +'set bufhidden=wipe'",
+            EDITOR = "nvr --remote-tab +'set bufhidden=wipe'",
+        },
     })
 
-    function _TOGGLE_LAZYGIT()
+    vim.api.nvim_create_user_command("ToggleLazygit", function()
+        ---@diagnostic disable-next-line: missing-parameter
         lazygit:toggle()
-    end
+    end, { desc = "Toggle floating lazygit git client" })
 
-    local whichkey = require("which-key")
-    whichkey.register({
-        g = {
-            name = "git",
-            l = { "<cmd>lua _TOGGLE_LAZYGIT()<CR>", "Lazygit" },
-        },
-    }, { prefix = "<Leader>" })
-    whichkey.register({
-        ["<C-t>"] = {
-            name = "Toggle Lazygit",
-            g = { "<cmd>lua _TOGGLE_LAZYGIT()<CR>", "Lazygit" },
+    vim.keymap.set("n", "<Leader>gl", "<cmd>ToggleLazygit<CR>", { desc = "Toggle floating lazygit git client" })
+    vim.keymap.set("n", "<C-t>g", "<cmd>ToggleLazygit<CR>", { desc = "Toggle floating lazygit git client" })
+end
+
+-- setup ranger toggleterm
+function M.ranger()
+    local ranger = require("toggleterm.terminal").Terminal:new({
+        cmd = "ranger",
+        direction = "float",
+        hidden = true,
+        env = {
+            -- open a new tab in current nvim to edit, instead of opening new vim in ranger
+            VISUAL = "nvr --remote-tab",
+            EDITOR = "nvr --remote-tab",
         },
     })
+
+    vim.api.nvim_create_user_command("ToggleRanger", function()
+        ---@diagnostic disable-next-line: missing-parameter
+        ranger:toggle()
+    end, { desc = "Toggle floating ranger file manager" })
+
+    vim.keymap.set("n", "<C-t>b", "<cmd>ToggleRanger<CR>", { desc = "Toggle floating ranger file manager" })
 end
 
 return M
