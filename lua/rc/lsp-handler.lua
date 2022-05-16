b8259eb7e2881ba2654141b6af6b91318f25bab2
Diego Fujii <android.mxdiego9@gmail.com>
2022-05-16 14:21:33 +0900

    update lsp-handler

--- lua/rc/lsp-handler.lua
+++ lua/rc/lsp-handler.lua
@@ -4,40 +4,38 @@ function M.on_attach(client, bufnr)
     local group = vim.api.nvim_create_augroup("LspFormatting", { clear = false })
     vim.api.nvim_create_autocmd("BufWritePre", {
         group = group,
-        ---@diagnostic disable-next-line: unused-local, redefined-local
-        callback = function(bufnr)
+        callback = function()
             vim.lsp.buf.format({
                 filter = function(clients)
-                    ---@diagnostic disable-next-line: unused-local, redefined-local
+                    ---@diagnostic disable: redefined-local
                     return vim.tbl_filter(function(client)
                         return client.name ~= "sumneko_lua" -- don't use sumneko_lua's formatter
                     end, clients)
                 end,
-                -- bufnr = bufnr,
-                -- temporary workaround for https://github.com/jose-elias-alvarez/null-ls.nvim/issues/582
-                -- switch this back once this is merged: https://github.com/neovim/neovim/pull/17075
-                bufnr = vim.api.nvim_get_current_buf(),
+                bufnr = bufnr,
             })
         end,
         desc = "Format buffer just before write",
-        buffer = 0,
+        buffer = bufnr,
     })
     vim.notify(
-        "lsp formater " .. client.name .. " set to buffer " .. vim.api.nvim_buf_get_name(0),
+        "lsp formater " .. client.name .. " set to buffer " .. bufnr .. ": " .. vim.api.nvim_buf_get_name(bufnr),
         vim.log.levels.TRACE
     )
 
     local group2 = vim.api.nvim_create_augroup("HoverDiagnostic", { clear = false })
     vim.api.nvim_create_autocmd("CursorHold", {
         group = group2,
-        ---@diagnostic disable-next-line: unused-local, redefined-local
-        callback = function(bufnr)
+        callback = function()
             vim.diagnostic.open_float(nil, { focusable = false })
         end,
         desc = "Open diagnostic flaoting window",
-        buffer = 0,
+        buffer = bufnr,
     })
-    vim.notify("CursorHold diagnostic autocmd set" .. vim.api.nvim_buf_get_name(0), vim.log.levels.TRACE)
+    vim.notify(
+        "CursorHold diagnostic autocmd set to buffer " .. bufnr .. ": " .. vim.api.nvim_buf_get_name(bufnr),
+        vim.log.levels.TRACE
+    )
 
     require("rc.plugins.config.telescope").keymap.lsp(bufnr)
 end
