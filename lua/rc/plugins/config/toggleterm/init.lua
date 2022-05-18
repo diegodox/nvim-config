b8b1bfa1c301963da0207c6f8d7666a6c87cd491
Diego Fujii <android.mxdiego9@gmail.com>
2022-05-18 16:40:30 +0900

    toggleterm module

---
+++ lua/rc/plugins/config/toggleterm/init.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+---Configure toggleterm.nvim
+function M.config()
+    if not vim.o.hidden then
+        vim.notify("toggleterm needs 'hidden' option, set hidden = true", vim.log.levels.WARN)
+        vim.o.hidden = true
+    end
+    require("toggleterm").setup({
+        start_in_insert = false,
+        float_opts = { winblend = 4 },
+    })
+end
+
+return M
