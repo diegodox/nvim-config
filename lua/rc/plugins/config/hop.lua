62304728b4888c9cd430a60be74f826d65f18153
Diego Fujii <android.mxdiego9@gmail.com>
2022-05-21 00:55:47 +0900

    hop change map

--- lua/rc/plugins/config/hop.lua
+++ lua/rc/plugins/config/hop.lua
@@ -2,8 +2,7 @@ local M = {}
 
 function M.config()
     require("hop").setup()
-    vim.keymap.set("n", "f", "<Cmd>HopChar1<CR>", { desc = "Hop to char after cursor" })
-    vim.keymap.set("n", "F", "<Cmd>HopChar1<CR>", { desc = "Hop to char before cursor" })
+    vim.keymap.set("n", "<C-f>", "<Cmd>HopChar1<CR>", { desc = "Hop to char" })
 end
 
 return M
