5e43006c23d847f31129514c4b0fb8a66a27bf3d
Diego Fujii <android.mxdiego9@gmail.com>
2022-05-20 23:56:29 +0900

    add pregister

--- lua/rc/plugins/config/which-key.lua
+++ lua/rc/plugins/config/which-key.lua
@@ -13,4 +13,20 @@ function M.config()
     whichkey.setup({})
 end
 
+---pcall `whichkey.register(mappings, opts)`
+---print `fail_msg` whenfail to `require("which-key")`
+---@param mappings any
+---@param opts any
+---@param fail_msg string?
+---@return boolean
+function M.pregister(mappings, opts, fail_msg)
+    local ok_whichkey, whichkey = pcall(require, "which-key")
+    if ok_whichkey then
+        whichkey.register(mappings, opts)
+    elseif fail_msg then
+        vim.notify_once("Plugin 'which-key' not found\n" .. fail_msg, vim.log.levels.WARN)
+    end
+    return ok_whichkey
+end
+
 return M
