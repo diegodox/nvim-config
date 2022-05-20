7e9087261afc4e1a2069bbac4d2429c1222bccbf
Diego Fujii <android.mxdiego9@gmail.com>
2022-05-21 00:39:39 +0900

    add null_ls tools

--- lua/rc/plugins/config/null-ls.lua
+++ lua/rc/plugins/config/null-ls.lua
@@ -17,7 +17,11 @@ function M.config()
     null_ls.setup({
         sources = {
             null_ls.builtins.formatting.stylua,
+            null_ls.builtins.formatting.fish_indent,
             null_ls.builtins.code_actions.gitsigns,
+            null_ls.builtins.diagnostics.actionlint,
+            null_ls.builtins.diagnostics.shellcheck,
+            null_ls.builtins.diagnostics.fish,
         },
         capabilities = lsphandler.capabilities(),
         on_attach = lsphandler.on_attach,
