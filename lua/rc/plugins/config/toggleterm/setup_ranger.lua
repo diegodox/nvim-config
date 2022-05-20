b3bc3a855b3b207b4ec90e40ff9b765f8b47bd5b
Diego Fujii <android.mxdiego9@gmail.com>
2022-05-21 00:04:14 +0900

    toggleterm keep ranger open

--- lua/rc/plugins/config/toggleterm/setup_ranger.lua
+++ lua/rc/plugins/config/toggleterm/setup_ranger.lua
@@ -3,6 +3,15 @@ return function()
     local ranger = require("rc.plugins.config.toggleterm.utils").new_terminal({
         cmd = "ranger",
         direction = "float",
+        on_open = function(term)
+            -- override ranger exit keybinding to stay open
+            vim.keymap.set(
+                "t",
+                "q",
+                "<cmd>ToggleRanger<CR>",
+                { desc = "Toggle floating ranger file manager", buffer = term.bufnr }
+            )
+        end,
     })
 
     vim.api.nvim_create_user_command("ToggleRanger", function()
