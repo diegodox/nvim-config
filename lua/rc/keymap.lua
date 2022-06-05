76bdde042aacb4b792b13119d1d2859bd2ba8da7
Diego Fujii <android.mxdiego9@gmail.com>
2022-06-06 03:41:29 +0900

    keymap <C-s> to save

--- lua/rc/keymap.lua
+++ lua/rc/keymap.lua
@@ -16,4 +16,9 @@ vim.keymap.set("n", "<right>", "<nop>", { desc = "No arrowkeys, use hjkl" })
 -- Esc from terminal
 vim.keymap.set("t", "<C-Space>", "<C-\\><C-n>", { desc = "Enter to Normal mode from Terminal mode" })
 
+-- Easy save
+vim.keymap.set("n", "<C-s>", function()
+    vim.cmd("update")
+end, { desc = "write buffer" })
+
 vim.cmd("source ~/.config/nvim/winresize.vim")
