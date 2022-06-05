d1c5e542cefd1581485394fd6a51fd10485f4ea3
Diego Fujii <android.mxdiego9@gmail.com>
2022-06-06 03:41:12 +0900

    refactor use "" instead of [[ ]]

--- lua/rc/keymap.lua
+++ lua/rc/keymap.lua
@@ -16,4 +16,4 @@ vim.keymap.set("n", "<right>", "<nop>", { desc = "No arrowkeys, use hjkl" })
 -- Esc from terminal
 vim.keymap.set("t", "<C-Space>", "<C-\\><C-n>", { desc = "Enter to Normal mode from Terminal mode" })
 
-vim.cmd([[source ~/.config/nvim/winresize.vim]])
+vim.cmd("source ~/.config/nvim/winresize.vim")
