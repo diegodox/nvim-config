dae5d83b8720a46a35d870c8a3b2aca58ea63577
Diego Fujii <android.mxdiego9@gmail.com>
2022-06-13 17:20:23 +0900

    fix rust-analyzer

--- lua/rc/plugins/config/lsp-installer.lua
+++ lua/rc/plugins/config/lsp-installer.lua
@@ -53,9 +53,14 @@ function M.config()
                     -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
                     settings = {
                         -- To enable rust-analyzer settings, visit: https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+                        -- Sometimes this is better resource: https://github.com/simrat39/rust-tools.nvim/wiki/Server-Configuration-Schema
                         ["rust-analyzer"] = {
-                            cargo = { features = "all" },
-                            checkOnSave = { command = "clippy", features = "all" },
+                            cargo = { allFeatures = true },
+                            checkOnSave = {
+                                command = "clippy",
+                                extraArgs = { "--examples", "--tests", "--benches" },
+                                allFeatures = true,
+                            },
                         },
                     },
                 }),
