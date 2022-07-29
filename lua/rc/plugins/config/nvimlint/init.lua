cc8f55d362609fa7f628dba79f901e5e58a6154f
Diego Fujii <android.mxdiego9@gmail.com>
2022-07-29 22:58:35 +0900

    add nvimlint

---
+++ lua/rc/plugins/config/nvimlint/init.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+function M.config()
+    local lint = require("lint")
+    lint.linters_by_ft = {
+    }
+    vim.api.nvim_create_autocmd("BufReadPost", {
+        desc = "Try run lint for new buffer",
+        group = vim.api.nvim_create_augroup("NvimLint", { clear = true }),
+        callback = function()
+            lint.try_lint()
+        end,
+    })
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        desc = "Try update lint for saved buffer",
+        group = vim.api.nvim_create_augroup("NvimLint", { clear = false }),
+        callback = function()
+            vim.notify("run lint!")
+            lint.try_lint()
+        end,
+    })
+end
+
+return M
