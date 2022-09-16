47592ee8ddd4cd0b53eb80e42ef90421dcab3975
Diego Fujii <android.mxdiego9@gmail.com>
2022-09-16 14:22:12 +0900

    gitsign add keymap

--- lua/rc/plugins/config/gitsigns.lua
+++ lua/rc/plugins/config/gitsigns.lua
@@ -10,6 +10,26 @@ function M.config()
         return
     end
 
+    local function next_hunk()
+        if vim.wo.diff then
+            return "]c"
+        end
+        vim.schedule(function()
+            gitsigns.prev_hunk()
+        end)
+        return "<Ignore>"
+    end
+
+    local function prev_hunk()
+        if vim.wo.diff then
+            return "[c"
+        end
+        vim.schedule(function()
+            gitsigns.prev_hunk()
+        end)
+        return "<Ignore>"
+    end
+
     require("rc.plugins.config.which-key").pregister({ g = { name = "Git" } }, { prefix = "<Leader>" })
 
     gitsigns.setup({
@@ -23,11 +43,21 @@ function M.config()
                 vim.notify_once("gs not loaded")
             end
 
+            vim.keymap.set("n", "]c", next_hunk, { desc = "Previous Git Hunk", buffer = bufnr })
+            vim.keymap.set("n", "[c", prev_hunk, { desc = "Next Git Hunk", buffer = bufnr })
+
             vim.keymap.set("n", "<Leader>gh", gs.preview_hunk, { desc = "Preview Hunk Diff", buffer = bufnr })
             vim.keymap.set("n", "<Leader>gs", gs.stage_hunk, { desc = "Stage Hunk", buffer = bufnr })
             vim.keymap.set("n", "<Leader>gR", gs.reset_hunk, { desc = "Reset Hunk", buffer = bufnr })
             vim.keymap.set("n", "<Leader>gu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk", buffer = bufnr })
-
+            vim.keymap.set("n", "<leader>gb", gs.blame_line, { desc = "Blame current line", buffer = bufnr })
+            vim.keymap.set("n", "<Leader>gd", gs.toggle_deleted, { desc = "Toggle deleted", buffer = bufnr })
+            vim.keymap.set(
+                "n",
+                "<Leader>gB",
+                gs.toggle_current_line_blame,
+                { desc = "Toggle current line git blame", buffer = bufnr }
+            )
             -- git hunk as text object
             vim.keymap.set(
                 { "o", "x" },
