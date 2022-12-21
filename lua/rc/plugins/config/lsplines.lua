ed160b1d4361102e648a63032d9f356875a71444
diegodox <android.mxdiego9@gmail.com>
2022-12-22 02:33:41 +0900

    remove unused fn

--- lua/rc/plugins/config/lsplines.lua
+++ lua/rc/plugins/config/lsplines.lua
@@ -49,57 +49,6 @@ function M.hide_diagnostic(name, should_fallback)
     end
 end
 
----@param name string
-function M.cargo_autocmd(name)
-    -- Because of namespace configuration has higher priority than finally global configuration,
-    -- we need to set this again.
-    vim.api.nvim_create_autocmd("InsertEnter", {
-        desc = "Hide virtual_lines when enter insert mode.",
-        group = vim.api.nvim_create_augroup("CargoCheckDiagnostic", { clear = true }),
-        pattern = "*.rs",
-        callback = function()
-            -- diagnostic_config_by_name({ virtual_lines = false }, "cargo_clippy")
-            M.hide_diagnostic(name)
-        end,
-    })
-
-    -- We need to turn-on virtual_lines when leave Insert Mode,
-    -- since we already turn-off virtual_lines when enter to Insert mode.
-    --
-    -- In addition, check buffer is not modified because diagnostic which
-    -- cargo-check generated make sence only if file is not modified.
-    vim.api.nvim_create_autocmd("ModeChanged", {
-        desc = "Show virtual_lines when leave insert mode and buffer is not modified.",
-        group = vim.api.nvim_create_augroup("CargoCheckDiagnostic", { clear = false }),
-        pattern = "i:*",
-        callback = function()
-            if vim.bo.filetype == "rust" and not vim.bo.modified then
-                -- diagnostic_config_by_name({ virtual_lines = true }, "cargo_clippy")
-                M.show_diagnostic(name, { virtual_lines = true })
-            end
-        end,
-    })
-
-    -- virtual_lines need to follow buffer Modified status,
-    -- since sometinmes buffer modified/saved without enter/leave Insert mode.
-    --
-    -- TODO: Should mode afects this behavior?
-    -- (more specific, does this event happen while in insert mode?)
-    vim.api.nvim_create_autocmd("BufModifiedSet", {
-        desc = "Make virtual_lines follw buffer modified status.",
-        group = vim.api.nvim_create_augroup("CargoCheckDiagnostic", { clear = false }),
-        pattern = "*.rs",
-        callback = function()
-            -- diagnostic_config_by_name({ virtual_lines = not vim.bo.modified }, "cargo_clippy")
-            if vim.bo.modified then
-                M.hide_diagnostic(name)
-            else
-                M.show_diagnostic(name, { virtual_lines = true })
-            end
-        end,
-    })
-end
-
 function M.highlight() vim.cmd("highlight! default link DiagnosticVirtualTextHint NonText") end
 
 return M
