f97a439e97f585359df0a8d0d617c44a75a213f0
diegodox <android.mxdiego9@gmail.com>
2022-12-22 02:29:46 +0900

    rust-tools refactor

--- lua/rc/plugins/config/rust-tools.lua
+++ lua/rc/plugins/config/rust-tools.lua
@@ -19,29 +19,40 @@ local function keymap(bufnr)
 end
 
 function M.setup()
-    local cfg = {
-        tools = { inlay_hints = { highlight = "NonText" } },
-        server = require("rc.plugins.config.lsp_config").server_opts("rust_analyzer", {
-            -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
-            settings = {
-                -- To enable rust-analyzer settings, visit: https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-                -- Sometimes this is better resource: https://github.com/simrat39/rust-tools.nvim/wiki/Server-Configuration-Schema
-                ["rust-analyzer"] = {
-                    cargo = { allFeatures = true },
-                    checkOnSave = {
-                        command = "clippy",
-                        extraArgs = { "--examples", "--tests", "--benches" },
-                        allFeatures = true,
-                    },
+    local server = require("rc.plugins.config.lsp_config").server_opts("rust_analyzer", {
+        -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+        settings = {
+            -- To enable rust-analyzer settings, visit: https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+            -- Sometimes this is better resource: https://github.com/simrat39/rust-tools.nvim/wiki/Server-Configuration-Schema
+            ["rust-analyzer"] = {
+                cargo = { allFeatures = true },
+                checkOnSave = {
+                    command = "clippy",
+                    extraArgs = { "--examples", "--tests", "--benches" },
+                    allFeatures = true,
                 },
             },
-        }),
-    }
-    local on_attach = cfg.server.on_attach
-    cfg.server.on_attach = function(client, bufnr)
+        },
+    })
+
+    -- wrap server.on_attach
+    local on_attach = server.on_attach
+    server.on_attach = function(client, bufnr)
         on_attach(client, bufnr)
         keymap(bufnr)
     end
+
+    local cfg = {
+        server = server,
+        tools = {
+            inlay_hints = { highlight = "NonText" },
+            hover_actions = {
+                max_width = 80,
+                max_height = 30,
+            },
+        },
+    }
+
     require("rust-tools").setup(cfg)
 end
 
