210735c0c789437b8484c3d14687dd1d43fc8358
diegodox <android.mxdiego9@gmail.com>
2022-12-28 16:15:14 +0900

    refactor gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -7,19 +7,15 @@ local M = {
 
 M.dependencies = { "nvim-lua/plenary.nvim" }
 
-function M.config()
-    local ok, gitsigns = pcall(require, "gitsigns")
+function keymap(bufnr)
+    require("plugins.which-key").pregister({ g = { name = "Git" } }, { prefix = "<Leader>" })
 
-    if not ok then
-        vim.notify_once("plugin 'gitsigns.nvim' not found", vim.log.levels.INFO)
-        return
-    end
-
+    local gs = require("gitsigns")
     local function next_hunk()
         if vim.wo.diff then
             return "]c"
         end
-        vim.schedule(function() gitsigns.prev_hunk() end)
+        vim.schedule(function() gs.prev_hunk() end)
         return "<Ignore>"
     end
 
@@ -27,47 +23,44 @@ function M.config()
         if vim.wo.diff then
             return "[c"
         end
-        vim.schedule(function() gitsigns.prev_hunk() end)
+        vim.schedule(function() gs.prev_hunk() end)
         return "<Ignore>"
     end
 
-    require("plugins.which-key").pregister({ g = { name = "Git" } }, { prefix = "<Leader>" })
+    vim.keymap.set("n", "]c", next_hunk, { desc = "Previous Git Hunk", buffer = bufnr })
+    vim.keymap.set("n", "[c", prev_hunk, { desc = "Next Git Hunk", buffer = bufnr })
 
-    gitsigns.setup({
+    vim.keymap.set("n", "<Leader>gh", gs.preview_hunk, { desc = "Preview Hunk Diff", buffer = bufnr })
+    vim.keymap.set("n", "<Leader>gs", gs.stage_hunk, { desc = "Stage Hunk", buffer = bufnr })
+    vim.keymap.set("n", "<Leader>gR", gs.reset_hunk, { desc = "Reset Hunk", buffer = bufnr })
+    vim.keymap.set("n", "<Leader>gu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk", buffer = bufnr })
+    vim.keymap.set("n", "<leader>gb", gs.blame_line, { desc = "Blame current line", buffer = bufnr })
+    vim.keymap.set("n", "<Leader>gd", gs.toggle_deleted, { desc = "Toggle deleted", buffer = bufnr })
+    vim.keymap.set(
+        "n",
+        "<Leader>gB",
+        gs.toggle_current_line_blame,
+        { desc = "Toggle current line git blame", buffer = bufnr }
+    )
+    -- git hunk as text object
+    vim.keymap.set({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk", buffer = bufnr })
+end
+
+-- local signs = {
+--     add = { numhl = "GitSignsAddLn" },
+--     change = { numhl = "GitSignsChangeLn" },
+--     delete = { numhl = "GitSignsDeleteLn" },
+-- }
+
+function M.config()
+    require("gitsigns").setup({
+        signs = signs,
         numhl = true,
         signcolumn = false,
         current_line_blame = false,
         current_line_blame_opts = { delay = 100 },
         sign_priority = 100,
-        on_attach = function(bufnr)
-            local gs = package.loaded.gitsigns
-            if not gs then
-                vim.notify_once("gs not loaded")
-            end
-
-            vim.keymap.set("n", "]c", next_hunk, { desc = "Previous Git Hunk", buffer = bufnr })
-            vim.keymap.set("n", "[c", prev_hunk, { desc = "Next Git Hunk", buffer = bufnr })
-
-            vim.keymap.set("n", "<Leader>gh", gs.preview_hunk, { desc = "Preview Hunk Diff", buffer = bufnr })
-            vim.keymap.set("n", "<Leader>gs", gs.stage_hunk, { desc = "Stage Hunk", buffer = bufnr })
-            vim.keymap.set("n", "<Leader>gR", gs.reset_hunk, { desc = "Reset Hunk", buffer = bufnr })
-            vim.keymap.set("n", "<Leader>gu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk", buffer = bufnr })
-            vim.keymap.set("n", "<leader>gb", gs.blame_line, { desc = "Blame current line", buffer = bufnr })
-            vim.keymap.set("n", "<Leader>gd", gs.toggle_deleted, { desc = "Toggle deleted", buffer = bufnr })
-            vim.keymap.set(
-                "n",
-                "<Leader>gB",
-                gs.toggle_current_line_blame,
-                { desc = "Toggle current line git blame", buffer = bufnr }
-            )
-            -- git hunk as text object
-            vim.keymap.set(
-                { "o", "x" },
-                "ih",
-                ":<C-U>Gitsigns select_hunk<CR>",
-                { desc = "Select Hunk", buffer = bufnr }
-            )
-        end,
+        on_attach = keymap,
     })
 end
 
